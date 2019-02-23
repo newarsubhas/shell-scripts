84acaadda0d26c47ee9968dba03bb269af857307
newarsubhas <37718824+newarsubhas@users.noreply.github.com>
2019-02-22 19:27:54 -0500

    Update create_dir.sh

--- create_dir.sh
+++ create_dir.sh
@@ -3,7 +3,8 @@ My_Dir=`pwd`
 echo "My working dir is ${My_Dir}" 
 mkdir ${My_Dir}/subhas 
 if [ -d ${My_Dir}/subhas ] 
-then echo "directory already exists" 
+then 
+echo "directory already exists" 
 else 
 echo "No such directory"
 fi
